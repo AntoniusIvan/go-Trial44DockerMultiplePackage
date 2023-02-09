7fe05d5bf8cc3391e778e9ae9d517a560cc6278e
Antonius Ivan <antonius.ivan@cbn.co.id>
2023-02-09 09:19:53 +0700

    sdaa

--- main.go
+++ main.go
@@ -18,9 +18,9 @@ func main() {
 	mux.HandleFunc("/TryMainPkg", Controllers.TryMainPkg)
 	//mux.HandleFunc("/TryContrPkg", TryContrPkg)
 
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":80801", mux)
 	if err != nil {
-		log.Println("There was an error listening on port :8080", err)
+		log.Println("There was an error listening on port :80801", err)
 	}
 
 }
